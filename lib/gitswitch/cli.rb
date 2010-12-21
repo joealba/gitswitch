eff15b885ae7ab81d339fe54a410cec626d7f03b
Joe Alba <joe@joealba.com>
2010-12-20 23:42:16 -0500

    The add function should NOT have a default tag

--- lib/gitswitch/cli.rb
+++ lib/gitswitch/cli.rb
@@ -71,7 +71,7 @@ class Gitswitch
     ######################################################################
     desc "add [TAG]", "Add a new tagged user entry"
     map ["-a","--add"] => :add
-    def add(tag = 'default')
+    def add(tag)
       gs = Gitswitch.new
 
       tag.gsub!(/\W+/,'')
